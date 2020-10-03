02e2989b099a3876e0f92f642b94fa07896969e5
NurIkhwanAnnafis <ikhwan@twiscoder.com>
2020-10-03 18:00:37 +0700

    add alghoritm

---
+++ Javascript/utopianTree.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the utopianTree function below.
+function utopianTree(n) {
+    console.log(n)
+    let result = 0;
+    
+    for(let i = 0;i <= n;i++){
+        if(i%2===0){
+            result += 1;
+        } else if(i%2===1){
+            result *= 2;
+        }
+    }
+    
+    return result;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = utopianTree(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
