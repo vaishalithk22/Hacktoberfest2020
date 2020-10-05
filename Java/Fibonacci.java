1dd36299986612ff85342061bfadd485cd22240f
ronnieMock <40700075+ronnieMock@users.noreply.github.com>
2020-10-05 22:50:16 +0400

    Added Dynamic Input Method using Buffered Reader

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -1,4 +1,39 @@
-class fibonacci {
+class Reader {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+
+
+    static void init(InputStream input) {
+        reader = new BufferedReader(
+                     new InputStreamReader(input) );
+        tokenizer = new StringTokenizer("");
+    }
+
+
+    static String next() throws IOException {
+        while ( ! tokenizer.hasMoreTokens() ) {
+
+
+            tokenizer = new StringTokenizer(
+                   reader.readLine() );
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+
+    	return Integer.parseInt( next() );
+    }
+	
+    static double nextDouble() throws IOException {
+        return Double.parseDouble( next() );
+    }
+}
+
+
+
+Class Fibonacci {
     public static long fib(int x) {
         if(x<2)
         return x;
@@ -12,7 +47,8 @@ class fibonacci {
     }
 
     public static void main(String args[]) {
-        int n=9;
+        Reader.init(System.in);
+        int n = Reader.nextInt();
         System.out.println("The "+n+"th Fibonacci number is: "+fib(n));
         
     }
