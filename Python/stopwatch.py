e776f3ffe832d3750a27eb15d15ac29ef20925c6
coderAbhii <62416129+coderAbhii@users.noreply.github.com>
2020-10-19 21:19:32 +0530

    created stopwatch.py
    This file works as a simple stopwatch.

---
+++ Python/stopwatch.py
@@ -0,0 +1,27 @@
+import time
+
+f=input('Enter any key to start: ')
+
+if f == 1:
+
+	s=time.time()
+
+else:
+
+	s=time.time()
+
+	
+
+e=input('Press any key to end..')
+
+if e==1 :
+
+	l=time.time()
+
+else:
+
+	l=time.time()
+
+	
+
+print('time is', l-s)
